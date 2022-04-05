44339b16f42d5fa91098e947a79255fe6616fde4
luzlloveras <luzlloveras7@gmail.com>
2022-04-04 21:14:20 -0300

    Add files via upload

---
+++ Clase 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa.cs
+{
+    class Program // clase ejecutora
+    {
+        static void Main(string[] args)
+        {
+            string nombre, apellido;
+            Console.WriteLine("ingrese nombre");
+            nombre = System.Console.ReadLine();
+            Console.WriteLine("ingrese apellido");
+            apellido = System.Console.ReadLine();
+            saludar(nombre, apellido);
+            System.Console.ReadKey();
+        }
+
+        static void saludar (string nombre, string apellido)
+        {
+            System.Console.WriteLine("Hola {0} {1} .como estas?", nombre, apellido); // escribo en pantalla
+        }
+
+        static int doble(int numero)
+        {
+            int resultado = numero * 2;
+            return resultado;
+        }
+
+    }
+}
---
+++ Clase 3/clase3claseElemental/Class1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase3claseElemental
+{
+    internal class cuenta
+    {
+        ulong CBU;
+        string cliente;
+        float saldo;
+
+        // CONSTRUCTORES
+        public cuenta(ulong CBU, string cliente, float saldo)
+        {
+            this.CBU = CBU;
+            this.cliente = cliente;
+            this.saldo = saldo;
+        }
+
+        public cuenta(ulong CBU, string cliente)
+        {
+            this.CBU = CBU;
+            this.cliente = cliente;
+            this.saldo = 0;
+        }
+
+        public cuenta()
+        {
+            this.CBU = 0;
+            this.cliente = "";
+            this.saldo = 0;
+        }
+
+
+        // SETTERS
+        public void setCBU(ulong CBU)
+        {
+            if (CBU >= 100 && CBU < 1000000 )
+            {
+                this.CBU = CBU;
+            }
+            else
+            {
+                this.CBU = 0;
+            }
+        }
+
+        public void setCliente(string nombre)
+        {
+            cliente = nombre;
+        }
+
+        public void setSaldo()
+        {
+            this.saldo = saldo;
+        }
+
+        // GETTERS
+        
+        public ulong getCBU()
+        {
+            return this.CBU;
+        }
+
+        public string getCliente()
+        {
+            return this.cliente;
+        }
+
+        public float getSaldo()
+        {
+            return this.saldo;
+        }
+
+
+        // FUNCIONALIDADES
+        public bool depositar(float saldoRecibido)
+        {
+            if (saldoRecibido < 0)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo += saldoRecibido;
+            }
+            return true;            
+        }
+
+        public bool extraer(float saldoExtraible)
+        {
+            if (saldo < saldoExtraible)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= saldoExtraible;
+            }
+            return true;
+        }
+
+        public string darDatos()
+        {
+            return "Nombre: " + this.cliente + ", CBU: " + this.CBU + ", Saldo en cuenta: " + this.saldo.ToString();
+        }
+
+    }
+
+
+
+}
